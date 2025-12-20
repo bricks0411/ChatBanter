e6bc50cc50d30010280ef8d6e8089bc3b4e62cae
Bricks0411 <1072391442@qq.com>
2025-12-21 01:08:12 +0800

    fixed

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 from astrbot.api.message_components import At, Plain, Node
 
+TRIGGERS_GOOD_NIGHT = {
+    "晚安",
+    "安",
+    "goodnight",
+    "Goodnight",
+    "晚安咯",
+    "wanan"
+}
+
 @register(
     "test_plugin", 
     "Bricks0411", 
@@ -17,13 +26,23 @@ class RussianRoulette(Star):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("晚安", alias = {'安', 'goodnight', 'Goodnight', '晚安咯'})
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 晚安 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
+    @filter.event_message_type(filter.EventMessageType.ALL)
+    async def goodNight(self, event: AstrMessageEvent):
+        """这是一个 晚安 指令"""                         # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        # message_str = event.message_str                 # 用户发的纯文本消息字符串
+        # message_chain = event.get_messages()            # 用户所发的消息的消息链 # from astrbot.api.message_components import *
+
+        user_name = event.get_sender_name()             # 发送消息的用户名称
+        text = event.message_str.strip()
+
+        if not text:
+            logger.info("空消息。")
+            return
+        
+        if not any(key in text for key in TRIGGERS_GOOD_NIGHT):
+            return
+
+        # logger.info(message_chain)                      # 日志输出
         result = (
             f"晚，晚安啦，{user_name}！\n"
             "别误会，我可不是担心你，只是……今天看你还算努力。\n"
@@ -34,7 +53,7 @@ class RussianRoulette(Star):
             "\n（小声）\n"
             "……晚安。要是做梦的话，也给我做个像样点的。"
         )
-        yield event.plain_result([result]) # 发送一条纯文本消息
+        yield event.plain_result(result) # 发送一条纯文本消息
 
     @filter.command("add")
     async def GetSum(self, event: AstrMessageEvent, a: int, b: int):
